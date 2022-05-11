dc44ca2599d7262d1b3aa329e77d702819bd7c2f
Tom Konidas <tomkonidas@protonmail.ch>
2022-05-11 18:03:11 -0400

    Add Schema dir

---
+++ lib/plexus/schema.ex
@@ -0,0 +1,25 @@
+defmodule Plexus.Schema do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+
+      alias __MODULE__
+
+      @type t :: %__MODULE__{}
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts [type: :utc_datetime_usec]
+
+      @spec new!(Enumerable.t()) :: t()
+      def new!(attrs \\ []) do
+        struct!(__MODULE__, attrs)
+      end
+
+      @spec new(Enumerable.t()) :: t()
+      def new(attrs \\ []) do
+        struct(__MODULE__, attrs)
+      end
+    end
+  end
+end
