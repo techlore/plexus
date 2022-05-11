15240f230a4ed6ba11623c1433178524f9be0461
Tom Konidas <tomkonidas@protonmail.ch>
2022-05-11 17:28:27 -0400

    Add Credo

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule Plexus.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
