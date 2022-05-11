2ef1c9be66752b6d2fc1301dda4a33c8d3870914
Tom Konidas <tomkonidas@protonmail.ch>
2022-05-11 17:18:47 -0400

    Add Dialyzer

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Plexus.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
