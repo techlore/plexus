a9bc34281f3267363eaf259efc419c81ab1d8981
Tom Konidas <tomkonidas@protonmail.ch>
2022-05-17 10:03:40 -0400

    Bump phoenix from 1.6.8 to 1.6.9

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Plexus.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.8"},
+      {:phoenix, "~> 1.6.9"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
