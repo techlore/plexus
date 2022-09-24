3ed6c855d6082b32fa86b5b7df2fb1f92a8ca122
Tom Konidas <tomkonidas@protonmail.ch>
2022-09-24 09:34:47 -0400

    Setup Scrivener

--- lib/plexus/repo.ex
+++ lib/plexus/repo.ex
@@ -1,5 +1,18 @@
 defmodule Plexus.Repo do
-  use Ecto.Repo,
-    otp_app: :plexus,
-    adapter: Ecto.Adapters.Postgres
+  use Ecto.Repo, otp_app: :plexus, adapter: Ecto.Adapters.Postgres
+  use Scrivener, page_size: 15
+
+  @typedoc """
+  What is returned from a `Retail.Repo.paginate/2` call.
+
+  A Page has 5 fields that can be accessed:
+
+    * entries
+    * page_number
+    * page_size
+    * total_entries
+    * total_pages
+
+  """
+  @type page(entry) :: Scrivener.Page.t(entry)
 end
