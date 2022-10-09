40b774414f3621a8d188739fef87409a5026fcf2
Tom Konidas <tomkonidas@protonmail.ch>
2022-10-09 11:09:54 -0400

    Bump Elixir to v1.14.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plexus.MixProject do
     [
       app: :plexus,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:boundary, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
