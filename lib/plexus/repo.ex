6ce339f22ece71a5b3c432098c4276b36912dc6e
Tom Konidas <tom@tomkonidas.com>
2023-02-25 17:39:25 -0500

    Add Scrivener

--- lib/plexus/repo.ex
+++ lib/plexus/repo.ex
@@ -1,5 +1,4 @@
 defmodule Plexus.Repo do
-  use Ecto.Repo,
-    otp_app: :plexus,
-    adapter: Ecto.Adapters.Postgres
+  use Ecto.Repo, otp_app: :plexus, adapter: Ecto.Adapters.Postgres
+  use Scrivener, page_size: 15
 end
--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Plexus.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:scrivener_ecto, "~> 2.0"}
     ]
   end
 
