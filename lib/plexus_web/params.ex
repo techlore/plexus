46a7707dde943f6989f461894f7823fbc640d24a
Tom Konidas <tom@tomkonidas.com>
2023-03-06 22:36:24 -0500

    Add defaults to normalize

--- lib/plexus_web/params.ex
+++ lib/plexus_web/params.ex
@@ -12,34 +12,45 @@ defmodule PlexusWeb.Params do
   @doc """
   Normalize external input params.
 
-  ## Supported Options
+  ## Options
 
     - required (boolean)
+    - default (any)
 
   ## Examples
 
       iex> PlexusWeb.Params.normalize(%{"name" => "Spock"}, %{
       ...>   name: {:string, required: true},
+      ...>   affiliation: {:string, default: "Starfleet"},
       ...>   date_of_birth: {:utc_datetime_usec, []}
       ...> })
-      {:ok, %{name: "Spock"}}
+      {:ok, %{name: "Spock", affiliation: "Starfleet"}}
 
   """
   @spec normalize(map(), schema()) :: {:ok, map()} | {:error, Ecto.Changeset.t()}
   def normalize(params, schema) do
-    %{types: types, permitted: permitted, required: required} =
-      Enum.reduce(schema, %{types: %{}, permitted: [], required: []}, fn
+    %{defaults: data, types: types, permitted: permitted, required: required} =
+      Enum.reduce(schema, %{defaults: %{}, types: %{}, permitted: [], required: []}, fn
         {field, {type, opts}}, acc ->
           acc
+          |> put_default(field, opts)
           |> put_type(field, type)
           |> put_permitted(field)
           |> put_required(field, opts)
       end)
 
-    {%{}, types}
+    {data, types}
     |> Ecto.Changeset.cast(params, permitted)
     |> Ecto.Changeset.validate_required(required)
     |> Ecto.Changeset.apply_action(:normalize)
+  end
+
+  defp put_default(acc, field, opts) do
+    if default = opts[:default] do
+      Map.update!(acc, :defaults, &Map.put(&1, field, default))
+    else
+      acc
+    end
   end
 
   defp put_type(acc, field, type) do
