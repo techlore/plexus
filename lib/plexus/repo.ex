c310b3f860d1fdf8166cc882b0558d58666d067a
Tom Konidas <tom@tomkonidas.com>
2023-06-24 11:42:34 -0400

    Update Elixir version

--- lib/plexus/repo.ex
+++ lib/plexus/repo.ex
@@ -23,7 +23,7 @@ defmodule Plexus.Repo do
   back if the lambda returns `{:error, reason}`. In both cases, the function
   returns the result of the lambda.
   """
-  @spec transact((() -> any()), keyword()) :: {:ok, any()} | {:error, any()}
+  @spec transact((-> any()), keyword()) :: {:ok, any()} | {:error, any()}
   def transact(fun, opts \\ []) do
     transaction(
       fn ->
