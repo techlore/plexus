9ead0a0e831326855bbaa7f4c7bbcc9b0df68204
Tom Konidas <tom@tomkonidas.com>
2023-07-01 10:12:06 -0400

    Bump mix deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plexus.MixProject do
     [
       app: :plexus,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
