8ad2d7e7fe81cff9e75d8c9229439f7de75d09a2
Tom Konidas <tom@konidas.ca>
2026-06-29 20:37:40 -0400

    Bump Elixir/Erlang version

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :plexus, Plexus.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "plexus_dev",
+  port: 5434,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
